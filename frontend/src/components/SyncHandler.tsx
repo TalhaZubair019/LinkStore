d1a688010fba55e5f203abd2970a64e04b2a9880
iamtalha19 <163001283+TalhaZubair019@users.noreply.github.com>
2026-03-17 15:45:19 +0500

    update

--- frontend/src/components/SyncHandler.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useSyncCartWishlist } from '../hooks/useSyncCartWishlist';
-
-export default function SyncHandler({ children }: { children: React.ReactNode }) {
-  useSyncCartWishlist();
-  return <>{children}</>;
-}
